840bd8de77cba050e9c8457ecbaa2fb821fafded
Himani <himani9.engg@gmail.com>
2022-02-21 19:58:57 +0530

    added delete item functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
-
+import "../style.css";
 const App = () => {
   const [inputList, setInputList] = useState([]);
   const [term, setTerm] = useState("");
 
-  const renderItems = inputList.map((listItem) => {
-    return <h3>{listItem}</h3>;
+  const deleteItem = (id) => {
+    setInputList((oldInput) => {
+      return oldInput.filter((arrElem, index) => {
+        return index !== id;
+      });
+    });
+  };
+  const renderItems = inputList.map((listItem, index) => {
+    return (
+      <div className="item" key={index} id={index.toString()}>
+        <button className="delete-button" onClick={() => deleteItem(index)}>
+          X
+        </button>
+        <h3>{listItem}</h3>
+      </div>
+    );
   });
 
   // return <div>{renderFruits}</div>;
   const addItem = () => {
     setInputList((oldValues) => {
-      return [...oldValues, term];
+      console.log("term", term);
+      if (term != "") {
+        return [...oldValues, term];
+      } else {
+        alert("enter a valid value");
+      }
     });
     setTerm("");
   };
